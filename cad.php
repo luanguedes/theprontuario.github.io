b7cda8fbb92be9e1cd13c475b1a317382aff1e9b
Luan <luan.guedes@edu.unipar.br>
2018-09-05 21:13:38 -0300

    Commit 3 - Tela de Cadastro de Pacientes - HTML

---
+++ cad.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Cadastro de Pacientes</title>
+</head>
+<body>
+    <h1>Cadastro de Pacientes</h1>
+    <form>
+      Nome:  <input type="text" name="nome"/> <br>
+      Sexo: <input type="text" name="sexo"/> <br>
+      Data de Nascimento: <input type="text" name="data"/> <br>
+      CNS:  <input type="text" name="nome"/> <br>
+      Nome da Mãe: <input type="text" name="nomedamae"/> <br>
+      Endereco: <input type="text" name="endereco"/> <br>
+      Cidade: <input type="text" name="cidade"/> <br>
+
+    </form>
+</body>
+</html>
