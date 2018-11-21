04b87d872b6835482c8f16464f8ec18388de73d0
Luan <luan.guedes@edu.unipar.br>
2018-11-21 20:46:28 -0200

    Commit 12 - CRUD - CREATE

--- conexao.php
+++ conexao.php
@@ -14,45 +14,48 @@
       private function __construct() {  
          try { 
             
-            self::$pdo = new PDO("mysql:host=" . HOST . "; dbname=" . DBNAME . "; charset=" . CHARSET . ";", USER, PASSWORD);  
+            self::$pdo = new PDO("mysql:host=" . HOST . "; charset=" . CHARSET . ";", USER, PASSWORD); self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $pdo->query("CREATE DATABASE IF NOT EXISTS" . DBNAME);
-            $pdo->query("CREATE TABLE `especialidade` IF NOT EXISTS(
-                `idesp` int(11) NOT NULL,
-                `especialidade` varchar(100) DEFAULT NULL,
-                `status` varchar(10) DEFAULT NULL,
-                `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                `data_alteracao` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-          )");
-           
-            $pdo->query("CREATE TABLE `paciente` IF NOT EXISTS(
-                `id` int(11) NOT NULL,
-                `nome` varchar(100) DEFAULT NULL,
-                `sexo` varchar(20) DEFAULT NULL,
-                `data_nascimento` date DEFAULT NULL,
-                `cns` varchar(20) DEFAULT NULL,
-                `nome_mae` varchar(100) DEFAULT NULL,
-                `endereco` varchar(20) DEFAULT NULL,
-                `telefone` varchar(20) DEFAULT NULL,
-                `status` varchar(10) DEFAULT NULL,
-                `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                `data_alteracao` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-          )");
+        self::$pdo->query("CREATE DATABASE IF NOT EXISTS thebanco");
+        self::$pdo = new PDO("mysql:host=" . HOST . "; dbname=" . DBNAME . "; charset=" . CHARSET . ";", USER, PASSWORD); 
+        self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       
 
-            $pdo->query("CREATE TABLE `profissional` IF NOT EXISTS(
-                `id` int(11) NOT NULL,
-                `nome` varchar(100) DEFAULT NULL,
-                `sexo` varchar(20) DEFAULT NULL,
-                `data_nascimento` date DEFAULT NULL,
-                `cns` varchar(50) DEFAULT NULL,
-                `telefone` varchar(20) DEFAULT NULL,
-                `idespecialidade` int(11) DEFAULT NULL,
-                `status` varchar(10) DEFAULT NULL,
-                `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                `data_alteracao` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-          )");
-      
+        
+        self::$pdo->query("CREATE TABLE IF NOT EXISTS especialidade(
+            idesp int(11) NOT NULL,
+            especialidade varchar(100) DEFAULT NULL,
+            status varchar(10) DEFAULT NULL,
+            data_cadastro timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            data_alteracao timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+      )");
+       
+        self::$pdo->query("CREATE TABLE IF NOT EXISTS paciente(
+            id int(11) NOT NULL,
+            nome varchar(100) DEFAULT NULL,
+            sexo varchar(20) DEFAULT NULL,
+            data_nascimento date DEFAULT NULL,
+            cns varchar(20) DEFAULT NULL,
+            nome_mae varchar(100) DEFAULT NULL,
+            endereco varchar(20) DEFAULT NULL,
+            telefone varchar(20) DEFAULT NULL,
+            status varchar(10) DEFAULT NULL,
+            data_cadastro timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            data_alteracao timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+      )");
+
+        self::$pdo->query("CREATE TABLE IF NOT EXISTS profissional(
+            id int(11) NOT NULL,
+            nome varchar(100) DEFAULT NULL,
+            sexo varchar(20) DEFAULT NULL,
+            data_nascimento date DEFAULT NULL,
+            cns varchar(50) DEFAULT NULL,
+            telefone varchar(20) DEFAULT NULL,
+            idespecialidade int(11) DEFAULT NULL,
+            status varchar(10) DEFAULT NULL,
+            data_cadastro timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            data_alteracao timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+      )");
             
          } catch (PDOException $e) {  
           print "Erro: " . $e->getMessage();  
